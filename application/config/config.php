2d0d3e4e69c7909cd18824b66e56607c19dcc3f3
Luis Cabrera Benito <contacto@parzibyte.me>
2018-10-12 11:16:47 -0500

    Zona horaria

--- application/config/config.php
+++ application/config/config.php
@@ -1,5 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+date_default_timezone_set('America/Mexico_City');
 
 /*
 |--------------------------------------------------------------------------
